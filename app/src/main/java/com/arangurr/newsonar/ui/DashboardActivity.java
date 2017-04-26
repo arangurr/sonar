e3fb57338b0c415a0f7a9d22fd94aed00d6d1347
Rodrigo A <arangurrdev@gmail.com>
2017-04-26 16:47:51 +0200

    Expand Collapse

--- app/src/main/java/com/arangurr/newsonar/ui/DashboardActivity.java
+++ app/src/main/java/com/arangurr/newsonar/ui/DashboardActivity.java
@@ -7,12 +7,20 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.transition.AutoTransition;
+import android.transition.Transition;
+import android.transition.Transition.TransitionListener;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import com.arangurr.newsonar.BuildConfig;
 import com.arangurr.newsonar.Constants;
@@ -31,6 +39,9 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
   private SharedPreferences.OnSharedPreferenceChangeListener mPreferenceChangeListener;
 
   private DashboardRecyclerAdapter mAdapter;
+  private RecyclerView mPollRecyclerView;
+
+  private MyItemAnimator mAnimator;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +55,16 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
       }
     };
 
-    RecyclerView pollRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_dashboard_polls);
+    mPollRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_dashboard_polls);
 
     if (BuildConfig.DEBUG) {
       findViewById(R.id.fab_dashboard_add_debug).setVisibility(View.VISIBLE);
     }
 
+    mAnimator = new MyItemAnimator();
     mAdapter = new DashboardRecyclerAdapter(PersistenceUtils.fetchAllPolls(this));
-    pollRecyclerView.setAdapter(mAdapter);
+    mPollRecyclerView.setAdapter(mAdapter);
+    mPollRecyclerView.setItemAnimator(mAnimator);
   }
 
   @Override
@@ -110,13 +123,79 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
     }
   }
 
-  class DashboardRecyclerAdapter extends
-      RecyclerView.Adapter<DashboardRecyclerAdapter.ViewHolder> {
+  private static class MyItemAnimator extends SlideInItemAnimator {
+
+    private boolean animateMoves = false;
+
+    MyItemAnimator() {
+      super();
+    }
+
+    void setAnimateMoves(boolean animateMoves) {
+      this.animateMoves = animateMoves;
+    }
+
+    @Override
+    public boolean animateMove(
+        RecyclerView.ViewHolder holder, int fromX, int fromY, int toX, int toY) {
+      if (!animateMoves) {
+        dispatchMoveFinished(holder);
+        return false;
+      }
+      return super.animateMove(holder, fromX, fromY, toX, toY);
+    }
+  }
+
+  class DashboardRecyclerAdapter extends RecyclerView.Adapter<DashboardRecyclerAdapter.ViewHolder> {
+
+    private static final int EXPAND = 0x1;
+    private static final int COLLAPSE = 0x2;
+
+    private final android.transition.Transition mExpandCollapse;
 
     private List<Poll> mPolls;
+    private int mExpandedPosition = RecyclerView.NO_POSITION;
 
     public DashboardRecyclerAdapter(List<Poll> polls) {
       mPolls = polls;
+
+      mExpandCollapse = new AutoTransition();
+      mExpandCollapse.setDuration(120);
+      mExpandCollapse.setInterpolator(AnimationUtils
+          .loadInterpolator(DashboardActivity.this, android.R.interpolator.fast_out_slow_in));
+      mExpandCollapse.addListener(new TransitionListener() {
+        @Override
+        public void onTransitionStart(Transition transition) {
+          mPollRecyclerView.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+              return true;
+            }
+          });
+        }
+
+        @Override
+        public void onTransitionEnd(Transition transition) {
+          mAnimator.setAnimateMoves(true);
+          mPollRecyclerView.setOnTouchListener(null);
+
+        }
+
+        @Override
+        public void onTransitionCancel(Transition transition) {
+
+        }
+
+        @Override
+        public void onTransitionPause(Transition transition) {
+
+        }
+
+        @Override
+        public void onTransitionResume(Transition transition) {
+
+        }
+      });
     }
 
     public void swapArray(ArrayList<Poll> newArray) {
@@ -130,20 +209,70 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
       notifyItemInserted(mPolls.size() - 1);
     }
 
+    private void setExpanded(ViewHolder holder, boolean isExpanded) {
+      holder.itemView.setActivated(isExpanded);
+      holder.mDeleteButton.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
+    }
+
     @Override
     public DashboardRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
       View inflatedView = LayoutInflater.from(viewGroup.getContext())
           .inflate(R.layout.item_dashboard, viewGroup, false);
-      return new ViewHolder(inflatedView);
+
+      final ViewHolder holder = new ViewHolder(inflatedView);
+
+      holder.itemView.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+          final int position = holder.getAdapterPosition();
+          if (position == RecyclerView.NO_POSITION) {
+            return;
+          }
+
+          TransitionManager.beginDelayedTransition(mPollRecyclerView, mExpandCollapse);
+          mAnimator.setAnimateMoves(false);
+
+          // There is an expanded item
+          if (mExpandedPosition != RecyclerView.NO_POSITION) {
+            setExpanded(holder, false); // Collapse the previously expanded item
+            notifyItemChanged(mExpandedPosition, COLLAPSE);
+          }
+          // Expand this item
+          if (mExpandedPosition != position) {
+            mExpandedPosition = position;
+            setExpanded(holder, true);
+            notifyItemChanged(position, EXPAND);
+          } else {
+            // Item was collapsed, only need to set the expanded position to NO_POSITION
+            mExpandedPosition = RecyclerView.NO_POSITION;
+          }
+        }
+      });
+      return holder;
     }
 
     @Override
     public void onBindViewHolder(DashboardRecyclerAdapter.ViewHolder viewHolder, int i) {
+      final int position = viewHolder.getAdapterPosition();
+      final boolean isExpanded = position == mExpandedPosition;
+
       viewHolder.mTitle.setText(mPolls.get(i).getPollTitle());
       Date date = new Date(mPolls.get(i).getStartDate());
       SimpleDateFormat sdf = new SimpleDateFormat("dd MMM HH:mm", Locale.getDefault());
       viewHolder.mSubtitle.setText(sdf.format(date));
       viewHolder.mCircle.setText(String.valueOf(i + 1));
+
+      setExpanded(viewHolder, isExpanded);
+    }
+
+    @Override
+    public void onBindViewHolder(DashboardRecyclerAdapter.ViewHolder holder, int position,
+        List<Object> payloads) {
+      if (payloads.contains(EXPAND) || payloads.contains(COLLAPSE)) {
+        setExpanded(holder, position == mExpandedPosition);
+      } else {
+        onBindViewHolder(holder, position);
+      }
     }
 
     @Override
@@ -156,6 +285,7 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
       private TextView mTitle;
       private TextView mSubtitle;
       private TextView mCircle;
+      private ImageButton mDeleteButton;
 
       public ViewHolder(View itemView) {
         super(itemView);
@@ -163,8 +293,9 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         mTitle = (TextView) itemView.findViewById(R.id.textview_dashboard_item_title);
         mSubtitle = (TextView) itemView.findViewById(R.id.textview_dashboard_item_subtitle);
         mCircle = (TextView) itemView.findViewById(R.id.textview_dashboard_item_circle);
+        mDeleteButton = (ImageButton) itemView.findViewById(R.id.button_dashboard_item_delete);
 
-        itemView.setOnClickListener(this);
+        //itemView.setOnClickListener(this);
       }
 
       @Override
